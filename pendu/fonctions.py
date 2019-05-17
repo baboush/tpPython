05b3adbe8a4a9f6e6d0c4a009e8329a66c106ddc
baboush <roche.arnaud@icloud.com>
2019-05-17 11:48:47 +0200

    fonction qui test si le fichier existe

--- pendu/fonctions.py
+++ pendu/fonctions.py
@@ -1,39 +1,16 @@
 """fonction du jeux du pendu"""
 
+import os
 import donnees
-from random import choices
-import pickle
-
-"""on crée le fichier score si il existe pas
-on lit et ajoute dans une bibliotheque"""
-def fichier(JOUEURS):
-    fichier = open("scores.txt", "rb")
-    liste_joueur = pickle.Unpickler(fichier)
-    JOUEURS = liste_joueur
-    fichier.close()
-
-"""on test si le joueur existe ou pas"""
-def nomJoueur(nom, JOUEURS):
-    for nom in JOUEURS.keys():
-        if nom is not JOUEURS.keys():
-            JOUEURS[nom] = 0
-        else:
-            print("Vous avez selectioné le joueur {}, votre score est de : {}"\
-                  .format(JOUEURS.keys(), JOUEURS.values()))
-
-def choixMots(MOTS):
-    mot_Trouver = choices(MOTS)
-    print("Le mot a trouver est de {} lettres".format(len(mot_Trouver)))
-    return mot_Trouver
-
-"""on ajoute le score au joueur
-on l'enregistre dans le fichier"""
-def ajoutScores(JOUEURS):
-    JOUEURS[nom] = JOUEURS.values() + NBR_CHANCES
-    with open("scores.txt", "wb") as fichier:
-        save = pickle.Pickler(fichier)
-        save.dump(JOUEURS)
-    fichier.close()
 
 
+def creerFichier():
+    if os.path.isfile("scores.txt") == False:
+        print("le fichier des scores n'existe pas")
+        print("creation du fichier scores")
+        fichier = open("scores.txt", "x")
+        fichier.close()
+    else:
+        print("Un ichier scores à été trouvé")
+        pass
 
