0e53decf4080aa007a991da61c2ca168ecb55591
Baboush <roche.arnaud@icloud.com>
2019-05-21 16:49:31 +0200

    ajout fonction randomMot

--- pendu/fonctions.py
+++ pendu/fonctions.py
@@ -1,18 +1,20 @@
 """fonction du jeux du pendu"""
-
 import os
-import donnees
 import pickle
+import random
+import donnees
+
 
 def creerFichier():
-    if os.path.isfile("scores.txt") == False:
+    if os.path.isfile("scores.txt"):  # regarde si le fichier existe
+        print("Un fichier scores à été trouvé")
+        pass
+    else:  # on crée le fichier s'il existe pas
         print("le fichier des scores n'existe pas")
         print("creation du fichier scores")
         fichier = open("scores.txt", "x")
         fichier.close()
-    else:
-        print("Un fichier scores à été trouvé")
-        pass
+
 
 def lireFichier():
     try:
@@ -23,9 +25,21 @@ def lireFichier():
     except EOFError:
         print("le fichier est vide")
         pass
-    
+
+
 def enregistrerJoueur(nom):
     with open("scores.txt", "ab") as fichier:
         ecriture = pickle.dump(nom, fichier)
+        print(ecriture)
     fichier.close()
 
+
+def randomMot():
+    mot = random.choice(donnees.MOTS)  # selection mot au hasard
+    liste = [i for i in mot]
+    index = 0
+    mot_cache = []
+    while index < len(liste):  # cache le mot
+        mot_cache.append('*')
+        index += 1
+    return liste, mot_cache
